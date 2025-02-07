2928b95019a89f507b440bf20f9f458f52e8ee6b
oscar <oscarsilla00@gmail.com>
2025-02-07 16:43:14 +0100

    feat: Initialize data in category names

--- driven/repository-sql/src/main/resources/db/migration/V2.0.0__initialize-data/V2.0.0__initialize-data.sql
+++ driven/repository-sql/src/main/resources/db/migration/V2.0.0__initialize-data/V2.0.0__initialize-data.sql
@@ -13,10 +13,25 @@ VALUES
 ('EXPENSE'),
 ('EXPENSE'),
 ('EXPENSE'),
-('EXPENSE'),
-('REVENUE'),
 ('REVENUE'),
 ('REVENUE'),
 ('REVENUE'),
 ('REVENUE'),
 ('REVENUE');
+COMMIT;
+
+/************************************
+* D_CATEGORY_NAMES
+************************************/
+INSERT INTO D_CATEGORY_NAMES (CATEGORY_ID, "name", LOCALE)
+VALUES
+(1, 'FOOD', 'en_EN'),
+(1, 'COMIDA', 'es_ES'),
+(2, 'WORK', 'en_EN'),
+(2, 'TRABAJO', 'es_ES'),
+(3, 'FINANCES', 'en_EN'),
+(3, 'FINANZAS', 'es_ES'),
+(4, 'HOME', 'en_EN'),
+(4, 'CASA', 'es_ES'),
+(5, 'ENTERTAINMENT', 'en_EN'),
+(5, 'ENTRETENIMIENTO', 'es_ES');
